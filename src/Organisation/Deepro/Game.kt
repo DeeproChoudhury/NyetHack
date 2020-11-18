f7baef3ed628f0596a440e955049e58084682ad1
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2020-11-18 10:28:29 +0000

    Refactored with when clause and added String Templates

--- src/Organisation/Deepro/Game.kt
+++ src/Organisation/Deepro/Game.kt
@@ -10,27 +10,22 @@ fun main(args: Array<String>) {
     //Aura
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColour = if (auraVisible) "GREEN" else "NONE"
-    println(auraColour)
 
-    val healthStatus = if (healthPoints == 100) {
+    val healthStatus = when (healthPoints) {
 
-            "is in excellent condition!"
-
-        } else if (healthPoints in 90..99) {
-            "has a few scratches."
-        } else if (healthPoints in 75..89) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 ->
             if (isBlessed) {
                 "has some minor wounds but is healing quite quickly!"
             } else {
                 "has some minor wounds."
             }
-        } else if (healthPoints in 15..74) {
-            "looks pretty hurt."
-        } else {
-            "is in awful condition!"
-        }
-    //Player Status
-    println(name + " " + healthStatus)
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
-
+    println("(Aura: $auraColour) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    //Player Status
+    println("$name $healthStatus")
+}
 
