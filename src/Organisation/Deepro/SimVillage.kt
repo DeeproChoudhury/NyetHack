e74e1d5a6ee8c05a78fc862ade6eed3b54369fee
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2020-11-19 03:00:58 +0000

    Added function references

--- src/Organisation/Deepro/SimVillage.kt
+++ src/Organisation/Deepro/SimVillage.kt
@@ -2,8 +2,8 @@ package Organisation.Deepro
 
 fun main(args: Array<String>) {
 
-        val greetingFunction = {
-                playerName: String, numBuildings: Int ->
+        runSimulation("Deepro", ::printConstructionCost) {
+                playerName, numBuildings ->
             val currentYear = 2020
             println("Adding $numBuildings houses")
             "Welcome to SimVillage, $playerName! (copyright $currentYear)"
@@ -11,10 +11,16 @@ fun main(args: Array<String>) {
         }
 
 
-    runSimulation("Deepro", greetingFunction)
+
 }
 
-fun runSimulation (playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation (playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..3).shuffled().last() //randomly selects 1, 2 or 3
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
