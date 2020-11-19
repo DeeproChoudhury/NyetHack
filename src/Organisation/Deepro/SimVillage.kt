06e9738dc39efbcf532d862acfdb67b1f9289eef
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2020-11-19 03:46:14 +0000

    Refactored with function return type, done

--- src/Organisation/Deepro/SimVillage.kt
+++ src/Organisation/Deepro/SimVillage.kt
@@ -2,23 +2,29 @@ package Organisation.Deepro
 
 fun main(args: Array<String>) {
 
-        runSimulation("Deepro", ::printConstructionCost) {
-                playerName, numBuildings ->
-            val currentYear = 2020
-            println("Adding $numBuildings houses")
-            "Welcome to SimVillage, $playerName! (copyright $currentYear)"
-
-        }
+        runSimulation()
 
 
 
 }
 
-inline fun runSimulation (playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last() //randomly selects 1, 2 or 3
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
+fun runSimulation () {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Deepro"))
+    println(greetingFunction("Deepro"))
 }
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return {playerName: String ->
+        val currentYear = 2020
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+}
+
 
 fun printConstructionCost(numBuildings: Int) {
     val cost = 500
